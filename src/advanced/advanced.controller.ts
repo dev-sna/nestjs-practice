7280e69df69403ed9cfcaf8949b01a43941ff62e
dev-sna <shahnawaz19415@gmail.com>
2019-09-19 02:18:19 +0500

    Controllers topic completed

--- src/advanced/advanced.controller.ts
+++ src/advanced/advanced.controller.ts
@@ -9,21 +9,53 @@ import {
   HttpCode,
   Body,
   Param,
+  Header,
+  Redirect,
 } from '@nestjs/common';
 import { CreateItemDTO } from './dto/create-item.dto';
+import { NextFunction } from 'express';
 
 @Controller('advanced')
 export class AdvancedController {
+  @Redirect('http://localhost/advanced/target', 302)
+  @Get('direct')
+  director(): any {
+    return { url: 'http://localhost:3000/advanced/target' };
+  }
+
+  @Get('target')
+  redirectTarget(): string {
+    return 'Redirected here';
+  }
+
   //   define subroute
-  @Get('not found')
+  @Get('not-found')
   @HttpCode(404)
   getData(): string {
     return 'Not found';
   }
 
+  // session
+  @Get('session')
+  session(@Session() session): string {
+    console.log('session: ', session);
+    return 'Session';
+  }
+
+  // next
+  @Get('next')
+  next(@Next() next: NextFunction): string {
+    // It won't return anything and will call the api defined below
+    next();
+    return 'Data has been updated';
+  }
+
+  @Header('Cache-Control', 'none')
   @Get(':id')
+  // findOne(@Param() params)
+  // another way to do the same
   findOne(@Param('id') id) {
-    return id;
+    return `ID: ${id}`;
   }
 
   @Post('create')
