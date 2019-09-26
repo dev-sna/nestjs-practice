ce8f883c341d1109d325f2b0f769e56204409664
dev-sna <shahnawaz19415@gmail.com>
2019-09-26 22:57:29 +0500

    Middleware completed(login)

--- src/core/core.controller.ts
+++ src/core/core.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get } from '@nestjs/common';
 @Controller('core')
 export class CoreController {
   @Get()
-  getDate() {
+  getData() {
     return 'Core module';
   }
 }
---
+++ src/middleware/login.middleware.ts
@@ -0,0 +1,11 @@
+// Middleware is a function called before route handler
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: Function) {
+    console.log('Request...', req);
+    next();
+  }
+}
