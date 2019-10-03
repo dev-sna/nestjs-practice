7190c0b4e934e1654b4f62be332e9a7213173888
dev-sna <shahnawaz19415@gmail.com>
2019-10-04 00:25:31 +0500

    Controller added

--- src/pipecontroller/pipecontroller.controller.ts
+++ src/pipecontroller/pipecontroller.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Post, Body, Get, UsePipes } from '@nestjs/common';
-import { CreateCarDTO } from './dto/createItem.dto';
+import { CreateCarDTO } from './dto/createCar.dto';
 import { PipeControllerService } from './pipecontroller.service';
 import { Car } from './pipecontroller.interfaces';
 import { JoiValidationPipe } from '../utils/pipes/joi.validation.pipe';
+import { ClassValidatorPipe } from '../utils/pipes/class-validator.pipe';
 import { createCarSchema } from './validation-schema/validation.schema';
+import { CreatePartDto } from './dto/createPart.dto';
 
 @Controller('pipe')
 export class PipeControllerController {
@@ -14,6 +16,13 @@ export class PipeControllerController {
   addCar(@Body() createCarDto: CreateCarDTO) {
     this.pipeService.createCar(createCarDto);
     return 'Car created successfully';
+  }
+
+  @Post('/part')
+  @UsePipes(new ClassValidatorPipe())
+  addPart(@Body() createPartDto: CreatePartDto) {
+    this.pipeService.createPart(createPartDto);
+    return 'Part added successfully';
   }
 
   @Get()
