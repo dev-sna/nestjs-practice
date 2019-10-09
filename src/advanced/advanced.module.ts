2b7e682cf7ebde34472811346bb5e1eb809589c0
dev-sna <shahnawaz19415@gmail.com>
2019-10-10 00:24:24 +0500

    Basic roles guard implemented

--- src/advanced/advanced.module.ts
+++ src/advanced/advanced.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AdvancedController } from './advanced.controller';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from '../utils/guards/basic-roles.guard';
 
 @Module({
   imports: [],
   controllers: [AdvancedController],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
 })
 export class AdvancedModule {}
